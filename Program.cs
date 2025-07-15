28e5868f6f47afd91d2fe987eb803ac5d05d25e7
VladislaV9 <vlad.nikitenkov95@gmail.com>
2025-07-15 14:56:57 +0300

    First

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+vlad.nikitenkov95@gmail.com
+public class FunctionsExample
+{
+    public static void Main(string [] args)
+    {
+        Console.WriteLine("");
+        Console.WriteLine("Здравствуй,");
+        Console.WriteLine("Василий!");
+        Console.WriteLine("");
+        Console.WriteLine("Здравствуй, Василий!");
+        Console.Write("Здравствуй, ");
+        Console.WriteLine("Василий!");
+        Console.WriteLine("");
+
+        string result = "";
+        for (int i = 0; i < 5; i++)
+        {
+            result += "*";
+            Console.WriteLine(result);
+        }
+        Console.WriteLine("");
+    }
+}
