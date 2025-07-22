d23d241ac7dfda78340917b4714e07cb383d6dd0
VladislaV9 <vlad.nikitenkov95@gmail.com>
2025-07-22 22:42:43 +0300

    еще задачи

--- Program.cs
+++ Program.cs
@@ -66,20 +66,76 @@ public class FunctionsExample
         // {
         //     System.Console.WriteLine("Доступ запрещен!");
         // }
-        System.Console.WriteLine("Первое чилсло");
-        int oneNumber = Convert.ToInt32(Console.ReadLine());
-        System.Console.WriteLine("Второе чилсло");
-        int twoNumber = Convert.ToInt32(Console.ReadLine());
+        // System.Console.WriteLine("Первое чилсло");
+        // int oneNumber = Convert.ToInt32(Console.ReadLine());
+        // System.Console.WriteLine("Второе чилсло");
+        // int twoNumber = Convert.ToInt32(Console.ReadLine());
 
-        if (oneNumber == twoNumber)
+        // if (oneNumber == twoNumber)
+        // {
+        //     System.Console.WriteLine("твои число " + oneNumber + " они имеют одинковое знаечение");
+        // } else if (oneNumber > twoNumber ){
+        //     System.Console.WriteLine(oneNumber);
+        // }else if (oneNumber < twoNumber ){
+        //     System.Console.WriteLine(twoNumber);
+        // }
+
+
+        // System.Console.WriteLine("Первое чилсло");
+        // int oneNumber = Convert.ToInt32(Console.ReadLine());
+
+        // if (oneNumber > 100 && oneNumber < 1000)
+        // {
+        //     System.Console.WriteLine("твои число " + oneNumber + " YES");
+        // }
+        // else
+        // {
+        //     System.Console.WriteLine("NO");
+        // }
+
+        // int first = oneNumber / 100;
+        // int second = oneNumber / 10 % 10;
+        // int thri = oneNumber / 1 % 10;
+
+        // System.Console.WriteLine(first);
+        // System.Console.WriteLine(second);
+        // System.Console.WriteLine(thri);
+
+        // if (first != second && second != thri && first != thri)
+        // {
+        //     System.Console.WriteLine("Yes");
+        // }
+        // else
+        // {
+        //     System.Console.WriteLine("NO");
+        // }
+
+        System.Console.WriteLine("номер билета");
+        int number = Convert.ToInt32(Console.ReadLine());
+        int first = number / 100000;
+        int second = number / 10000 % 10;
+        int thri = number / 1000 % 10;
+        int four = number / 100 % 10;
+        int five = number / 10 % 10;
+        int six = number / 1 % 10;
+
+        int sum = first + second + thri;
+        int sumSecond = four + five + six;
+
+        System.Console.WriteLine(first);
+        System.Console.WriteLine(second);
+        System.Console.WriteLine(thri);
+        System.Console.WriteLine(four);
+        System.Console.WriteLine(five);
+        System.Console.WriteLine(six);
+
+        if (sum == sumSecond)
         {
-            System.Console.WriteLine("твои число " + oneNumber + " они имеют одинковое знаечение");
-        } else if (oneNumber > twoNumber ){
-            System.Console.WriteLine(oneNumber);
-        }else if (oneNumber < twoNumber ){
-            System.Console.WriteLine(twoNumber);
+            System.Console.WriteLine("Yes");
         }
-
+        else
+        {
+            System.Console.WriteLine("No");
+        }
     }
-
 }
--- obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
+++ obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fdeb4715ddf31f452d18fd95f1d52d718d27355c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab87e51d0cf3738da62ebc1a0e54309b0a9fbb82")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
